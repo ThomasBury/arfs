94e7b450e9fe39c78c571b79dd1f9e20907fb034
Thomas Bury <thomas.bury@allianz.be>
2022-02-08 12:20:50 +0100

    update sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,17 @@ release = '0.3.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
+    # 'numpydoc',
+    'sphinx_copybutton',
+    'sphinx_rtd_theme',
+    'nbsphinx',
+    'nbsphinx_link',
+    'sphinx_tabs.tabs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +58,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_title = 'ARFS Documentation'
+html_show_sourcelink = True
