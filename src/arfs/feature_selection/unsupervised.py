correct most_collinear_series calculation

src/arfs/feature_selection/unsupervised.py
@@ -433,14 +433,20 @@ def _most_collinear(association_matrix, threshold):
     to_drop = list(set(cols_to_drop).union(set(rows_to_drop)))
     if not to_drop:
         return None, None
+    # for features in `to_drop` sum up their column and row values to find
+    # the most collinear feature
     most_collinear_series = (
-        association_matrix[to_drop].abs().sum(axis=1).sort_values(ascending=False)
+        association_matrix.loc[:, to_drop]
+        .abs()
+        .sum(axis=0)
     )
     most_collinear_series += (
-        association_matrix[to_drop].abs().sum(axis=0).sort_values(ascending=False)
+        association_matrix.loc[to_drop, :]
+        .abs()
+        .sum(axis=1)
     )
     most_collinear_series /= 2
-    return most_collinear_series.index[0], to_drop
+    return most_collinear_series.sort_values(ascending=False).index[0], to_drop
 
 
 def _recursive_collinear_elimination(association_matrix, threshold):