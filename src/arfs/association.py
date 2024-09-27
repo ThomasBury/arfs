978b2309a8797e1b41311ba756dbaafa1a918a03
Thomas Bury <bury.thomas@gmail.com>
2024-09-27 22:42:49 +0200

    fix: :bug: fix the pandas syntax to avoid error with the newer version

--- src/arfs/association.py
+++ src/arfs/association.py
@@ -1545,7 +1545,11 @@ def f_cat_regression_parallel(X, y, sample_weight=None, n_jobs=1, handle_na="dro
     X, sample_weight = _check_association_input(X, sample_weight, handle_na)
 
     y = X[target].copy()
-    X = X.drop(target, axis=1)
+    
+    # Ensure 'target' is a valid column in X
+    if target in X.columns:
+        X = X.drop(columns=[target])
+
 
     # define the number of cores
     n_jobs = min(cpu_count(), X.shape[1]) if n_jobs == -1 else min(cpu_count(), n_jobs)
